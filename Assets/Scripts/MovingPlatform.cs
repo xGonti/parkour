dd05a1b5fc7285dcba7d31882032b34668723e7d
xGonti <79505615+xGonti@users.noreply.github.com>
2022-05-10 21:20:56 +0200

    update

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,41 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+// Av Michal
+// Checkpoint = Ett eller flera stället som platformen ska röra sig till
+public class MovingPlatform : MonoBehaviour
 {
-    public float speed;
-    public int startingPoint;
-    public Transform[] points;
+    public float speed; // Hur snabbt platform rör sig
+    public int startingPoint; // Start position där platformen startar
+    public Transform[] points; // Positions där platformen ska röra sig till
 
     private int i;
-    // Start is called before the first frame update
     void Start()
     {
-        transform.position = points[startingPoint].position;
+        transform.position = points[startingPoint].position; // Ställer den till ena positionen med anvädning av "startingPoint"
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
+        if (Vector2.Distance(transform.position, points[i].position) < 0.02f) // Kollar distansen mellan platformen och "checkpoint" där den ska åka till
         {
-            i++;
-            if (i == points.Length)
+            i++; // Höjer index
+            if (i == points.Length) // Kollar ifall platformen var på sista "checkpoint" innan den höjer index
             {
-                i = 0;
+                i = 0; // Reset av index
             }
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime); // Flyttar platform till checkpoint till positionen med index "i"
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.transform.SetParent(transform);
+        collision.transform.SetParent(transform); // När man hoppar upp på platformen så åker spelaren med platformen (Det blir en child object till platformen)
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        collision.transform.SetParent(null);
+        collision.transform.SetParent(null); // När spelaren hoppar av platformen så åker den inte med platformen (Den blir inte längre en child object till platformen)
     }
 }
